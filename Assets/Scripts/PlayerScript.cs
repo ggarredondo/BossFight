df79d8542bb061cd72ae99a8e279971053af0271
Guillermo García <55685909+ggarredondo@users.noreply.github.com>
2021-10-27 12:01:42 +0100

    Awkard falling animation

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -8,17 +8,16 @@ public class PlayerScript : MonoBehaviour
     public Animator anim;
     public CharacterController controller;
 
-    public float walk_min = 0f, walk_max = 0.5f;
+    public float speed = 1f, walk_range_min = 0f, walk_range_max = 0.5f;
     public float turn_smoothness = 0.2f;
     public float jump_height = 2f;
     public float jump_cooldown = 1f;
     public float gravity = 11.81f;
 
-    private float horizontal, vertical, move_magnitude,
+    private float horizontal, vertical, move_magnitude, final_speed,
         turn_smooth_velocity, target_angle, rotation_angle; // unlocked movement variables
     private Vector3 direction, move_dir;
     private float dist_to_ground, jump_time; // jumping variables
-    private Vector3 jump_dir;
     private bool is_moving, is_walking, is_sprinting, is_jumping, is_grounded, is_locked; // animator variables
 
     private void Start() {
@@ -33,10 +32,6 @@ public class PlayerScript : MonoBehaviour
 
     private void UnlockedMovement()
     {
-        // base movement
-        horizontal = Input.GetAxis("Horizontal");
-        vertical = Input.GetAxis("Vertical");
-        direction.Set(horizontal, 0f, vertical);
         move_magnitude = direction.sqrMagnitude;
         direction = direction.normalized;
 
@@ -49,11 +44,8 @@ public class PlayerScript : MonoBehaviour
         move_dir = (Quaternion.Euler(0f, target_angle, 0f) * Vector3.forward).normalized; // movement relative to the camera
 
         // base movement phases: walking, running and sprinting
-        is_walking = move_magnitude >= walk_min && move_magnitude < walk_max && is_moving && !is_sprinting;
+        is_walking = move_magnitude >= walk_range_min && move_magnitude < walk_range_max && is_moving && !is_sprinting;
         is_sprinting = Input.GetButton("Sprint") && is_moving && !is_walking;
-
-        // final movement
-        controller.Move(move_dir * direction.magnitude * Time.deltaTime);
     }
 
     private void Jump()
@@ -61,23 +53,20 @@ public class PlayerScript : MonoBehaviour
         // jumping (cooldown between jumps) and falling
         is_grounded = IsGrounded();
         is_jumping = Input.GetButtonDown("Jump") && is_grounded && jump_time <= Time.time;
-        jump_dir = Vector3.zero;
-        if (is_jumping)
-        {
+        if (is_jumping) {
             jump_time = Time.time + jump_cooldown;
-            jump_dir.y = jump_height;
+            move_dir.y += jump_height;
         }
         else if (!is_grounded)
-            jump_dir += Physics.gravity * Time.deltaTime;
-        Debug.Log("button: " + Input.GetButtonDown("Jump"));
-        Debug.Log("is_grounded: " + is_grounded);
-        Debug.Log("jump_time <= Time.time: " + (jump_time <= Time.time));
-        Debug.Log("jump_dir: " + jump_dir);
-        controller.Move(jump_dir * Time.deltaTime);
+            move_dir += Physics.gravity * Time.deltaTime;
+        Debug.Log("button: " + Input.GetButtonDown("Jump")); // <- remove
+        Debug.Log("is_grounded: " + is_grounded); // <- remove
+        Debug.Log("jump_time <= Time.time: " + (jump_time <= Time.time)); // <- remove
     }
 
     private void Animation()
     {
+        anim.SetFloat("speed", speed);
         anim.SetFloat("horizontal", horizontal);
         anim.SetFloat("vertical", vertical);
         anim.SetBool("is_locked", is_locked);
@@ -90,8 +79,17 @@ public class PlayerScript : MonoBehaviour
 
     void Update()
     {
+        // basic input
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
+        direction.Set(horizontal, 0f, vertical);
+
         UnlockedMovement();
         Jump();
         Animation();
+
+        // final movement
+        Debug.Log("move_dir: " + move_dir);
+        controller.Move(move_dir * direction.magnitude * Time.deltaTime);
     }
 }
