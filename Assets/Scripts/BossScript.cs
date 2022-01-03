e04fabbab5f8c99f698ffead0385b78d1d0568d4
Rullow <rullowgl@gmail.com>
2022-01-04 00:49:54 +0100

    Boss follows the player

--- Assets/Scripts/BossScript.cs
+++ Assets/Scripts/BossScript.cs
@@ -9,34 +9,33 @@ public class BossScript : MonoBehaviour
 
     Animator anim;
     float distance, target_angle, rotation_angle, turn_smooth_velocity;
-    bool no_movement;
+    bool is_moving = false, is_following = false;
 
     private void Start()
     {
         anim = GetComponent<Animator>();
-        no_movement = false;
     }
 
     private void Movement() 
     {
         target_angle = Quaternion.LookRotation(player_pos.transform.position - transform.position).eulerAngles.y;
         rotation_angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, target_angle, ref turn_smooth_velocity, turn_smoothness);
-        if (!no_movement)
+        is_moving = is_following;
+        if (is_moving)
             transform.rotation = Quaternion.Euler(0f, rotation_angle, 0f);
     }
 
     private void Animation() 
     {
         anim.SetBool("out_of_combat", distance >= combat_distance);
-        anim.SetBool("is_following", false);
+        is_following = distance >= combat_distance && distance <= follow_distance;
+        anim.SetBool("is_following", is_following);
     }
 
     void Update()
     {
-        no_movement = anim.GetCurrentAnimatorStateInfo(0).IsName("Noncombat Idle") || anim.GetCurrentAnimatorStateInfo(0).IsName("Equip")
-            || anim.GetCurrentAnimatorStateInfo(0).IsName("Unequip") || anim.GetCurrentAnimatorStateInfo(0).IsName("Combat Idle");
         distance = Vector3.Distance(transform.position, player_pos.transform.position);
-        Debug.Log(distance);
+        Debug.Log(distance); // <------------- delete
 
         Animation();
         Movement();
